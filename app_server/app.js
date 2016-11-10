7a5067c151f148be4e9f45f9737b70c4bdd1c023
Anand Varma <anand@Anands-MacBook-Pro.local>
2016-11-10 18:16:18 -0500

    basic load-balancing

--- app_server/app.js
+++ app_server/app.js
@@ -1,4 +1,4 @@
-var nr = require('newrelic')
+var nr = require('newrelic');
 var redis = require('redis')
 var multer  = require('multer')
 var express = require('express')
@@ -7,17 +7,19 @@ var http      = require('http');
 var httpProxy = require('http-proxy')
 var app = express()
 // REDIS
-var client = redis.createClient(6379, '127.0.0.1', {})
+
+var redisIP = '104.131.119.23'
+var client = redis.createClient(6379, redisIP, {})
 
 var options = {};
 
 ///////////// WEB ROUTES
 
 // Add hook to make it easier to get all visited URLS.
-app.use(function(req, res, next) 
+app.use(function(req, res, next)
 {
-	console.log(req.method, req.url);
-	next(); // Passing the request to the next handler in the stack.
+        console.log(req.method, req.url);
+        next(); // Passing the request to the next handler in the stack.
 });
 
 app.get('/', function(req, res) {
@@ -32,5 +34,8 @@ var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  client.lpush("servers", "http://"+ownIP+":"+port , function(err, data){
+      console.log('Example app listening at http://%s:%s', host, port)
+  });
+
 });
