c38b3a95d5bafab2d3d028e23e716098d5a85765
Anand Varma <anand@Anands-MacBook-Pro.local>
2016-11-10 17:49:44 -0500

    hello world master server

---
+++ master_node/deploy.js
@@ -0,0 +1,36 @@
+var nr = require('newrelic')
+var redis = require('redis')
+var multer  = require('multer')
+var express = require('express')
+var fs      = require('fs')
+var http      = require('http');
+var httpProxy = require('http-proxy')
+var app = express()
+// REDIS
+var client = redis.createClient(6379, '127.0.0.1', {})
+
+var options = {};
+
+///////////// WEB ROUTES
+
+// Add hook to make it easier to get all visited URLS.
+app.use(function(req, res, next) 
+{
+	console.log(req.method, req.url);
+	next(); // Passing the request to the next handler in the stack.
+});
+
+app.get('/', function(req, res) {
+  res.send('<h2>hello world</h2>')
+});
+
+
+
+// HTTP SERVER
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+});
