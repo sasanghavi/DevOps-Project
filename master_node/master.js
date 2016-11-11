380cbbed491e0655406038524fdbeb0009fec5fa
Anand Varma <anand@Anands-MacBook-Pro.local>
2016-11-10 23:18:44 -0500

    master save redis IP

--- master_node/master.js
+++ master_node/master.js
@@ -13,6 +13,48 @@ var app = express()
 var redisIP = '127.0.0.1'
 var client = redis.createClient(6379, redisIP, {})
 
+
+///////// SELF IP //////////////
+var myIP;
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      //console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+      myIP = iface.address
+    }
+    ++alias;
+  });
+});
+
+
+fs.unlink('/root/redisIP', function(err){
+    // Ignore error if no file already exists
+    if (err && err.code !== 'ENOENT')
+        throw err;
+
+    var options = { flag : 'w' };
+    fs.writeFile('/root/redisIP', ""+redisIP , options, function(err) {
+        if (err) throw err;
+        console.log('file saved');
+    });
+});
+
+
+
 ///////////// WEB ROUTES
 
 // Add hook to make it easier to get all visited URLS.
