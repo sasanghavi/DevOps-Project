d4d8264da1a010183a0bd213a3b3a38884328784
Anand Varma <anand@Anands-MacBook-Pro.local>
2016-11-10 19:48:00 -0500

    Master - provision VM, run playbook

--- master_node/master.js
+++ master_node/master.js
@@ -5,6 +5,8 @@ var express = require('express')
 var fs      = require('fs')
 var http      = require('http');
 var httpProxy = require('http-proxy')
+var needle = require("needle");
+const exec = require('child_process').exec;
 var app = express()
 // REDIS
 
@@ -28,7 +30,64 @@ app.get('/', function(req, res) {
 
 
 app.get('/deploy', function(req, res) {
-  res.send('<h2>Got new code. Deploy!</h2>')
+    res.send('<h2>Got new code. Deploy!</h2>')
+
+    var name = "AutoScaling APP";
+	var region = "nyc2";
+	var image = "centos-6-5-x64";
+	client.createDroplet(name, region, image, function(err, resp, body)
+	{
+		if(!err && resp.statusCode == 202)
+		{
+			var dropletId = resp.body.droplet.id;
+			console.log("Got droplet: " + dropletId + " polling for public IP...");
+
+			// Get IP Handler
+			function getIPCallback(error, response)
+			{
+				var data = response.body;
+				if( (data.droplet.networks.v4.length > 0)  && (data.droplet.status == "active") )
+				{
+					//console.log(data.droplet.networks.v4[0].ip_address);
+					pubIP_DO = data.droplet.networks.v4[0].ip_address;
+
+					// Run Ansible Playbook here
+					console.log("ALL DONE!.." + pubIP_DO + "... Run ansible now!")  
+					invStr = "node0 ansible_ssh_host="+ pubIP_DO +" ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa\n"
+					fs.writeFile("inventory", invStr, function(err) {
+					    if(err) {
+					        return console.log(err);
+					    }
+					    console.log("Ansible Inventory file generated, running playbook!");
+
+					    // Run playbook
+	  					exec('ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory ../playbook.yml',
+						  function (error, stdout, stderr) {
+						    console.log('stdout: ' + stdout);
+						    console.log('stderr: ' + stderr);
+						    if (error !== null) {
+						      console.log('exec error: ' + error);
+						    }
+						});
+
+					}); 
+
+					// Run Playbook
+
+
+
+				} else {
+					console.log("...");
+					setTimeout(function () {
+			        	client.getIP(dropletId, getIPCallback);
+			    	}, 1000);
+				}
+			}
+
+			// Get IP
+			client.getIP(dropletId, getIPCallback);
+		}
+	});
 });
 
 
@@ -42,3 +101,70 @@ var server = app.listen(2000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 });
+
+
+
+
+
+
+// DIGITAL OCEAN - Client
+var headers =
+{
+	'Content-Type':'application/json',
+	Authorization: 'Bearer ' +  process.env.DO_TOKEN
+};
+
+
+var client =
+{
+	listRegions: function( onResponse )
+	{
+		needle.get("https://api.digitalocean.com/v2/regions", {headers:headers}, onResponse)
+	},
+
+
+	listImages: function( onResponse )
+	{
+		needle.get("https://api.digitalocean.com/v2/images", {headers:headers}, onResponse)
+	},
+
+
+	createDroplet: function (dropletName, region, imageName, onResponse)
+	{
+		var data = 
+		{
+			"name": dropletName,
+			"region":region,
+			"size":"512mb",
+			"image":imageName,
+			// Id to ssh_key already associated with account.
+			"ssh_keys":[3407276],
+			//"ssh_keys":null,
+			"backups":false,
+			"ipv6":false,
+			"user_data":null,
+			"private_networking":null
+		};
+
+		console.log("Attempting to create Droplet: "+ JSON.stringify(data) + "\n" );
+
+		needle.post("https://api.digitalocean.com/v2/droplets", data, {headers:headers,json:true}, onResponse );
+	},
+
+
+	getIP: function(dropletId, onResponse )
+	{
+		needle.get("https://api.digitalocean.com/v2/droplets/"+dropletId, {headers:headers}, onResponse)
+	},
+
+
+	deleteDroplet: function (dropletId, onResponse)
+	{
+		var data = null;
+
+		console.log("Attempting to delete: "+ dropletId );
+
+		needle.delete("https://api.digitalocean.com/v2/droplets/"+dropletId, data, {headers:headers,json:true}, onResponse );
+	},
+
+};
